317f65546e51a77fb80b1d3648212ca209cf4aae
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-12-04 11:33:12 -0600

    Removed un-used code on Ingredients page and Fixed Typo on MealsByName

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -28,7 +28,7 @@ onMounted(() => {
 
 <template>
     <div class="p-8 pb-0 text-orange-500">
-        <h1 class="text-4xl font-bold mb-4"> Search Meals By Letter</h1>
+        <h1 class="text-4xl font-bold mb-4"> Search Meals By Name</h1>
     </div>
 
     <div class="p-8">
